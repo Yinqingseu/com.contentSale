ced5bbbd4ee309893d91e64c70355c555a5ba6ac
yinqing <220150763@seu.edu.cn>
2018-02-27 13:09:44 +0800

    数据表更新

--- sql/insert_contents.sql
+++ sql/insert_contents.sql
@@ -1,3 +1,3 @@
  use content_sales_system;
  insert into contents(price,title,pic,summary,detail) 
-		  values(300,'title3','pic3','summary3','detail3');
+		  values(200,'title','pic','summary','detail');
