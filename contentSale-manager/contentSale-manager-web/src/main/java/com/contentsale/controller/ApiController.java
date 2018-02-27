67746ef7b8370e44c94faa0ea91a94c4491653be
yinqing <220150763@seu.edu.cn>
2018-02-28 00:09:38 +0800

    fileup,待设置web虚拟路径

--- contentSale-manager/contentSale-manager-web/src/main/java/com/contentsale/controller/ApiController.java
+++ contentSale-manager/contentSale-manager-web/src/main/java/com/contentsale/controller/ApiController.java
@@ -1,5 +1,10 @@
 package com.contentsale.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.contentsale.service.UserService;
 
@@ -36,12 +42,12 @@ public class ApiController {
 			map.addAttribute("code",200);
 			map.addAttribute("message","success");
 			map.addAttribute("result",true);
-			System.out.println("aaaaaaa");
+//			System.out.println("aaaaaaa");
 		}else {
 			map.addAttribute("code",401);
 			map.addAttribute("message", "账号或密码错误");
 			map.addAttribute("result",false);
-			System.out.println("bbbbbbbbbbb");
+//			System.out.println("bbbbbbbbbbb");
 		}
 		return "json";
 	}
@@ -50,10 +56,38 @@ public class ApiController {
 	/**
 	 *  图片上传
 	 * @return
+	 * @throws IOException 
+	 * @throws IllegalStateException 
 	 */
 	@RequestMapping("/upload")
-	public String uploadPic() {
-		return "";
+	@ResponseBody
+	public Model uploadPic(@RequestParam("file") MultipartFile file,Model map,
+			HttpServletRequest request) throws IllegalStateException, IOException {
+		//		文件存放服务端位置
+		String path = request.getSession().getServletContext().getRealPath("/uploadImgs");
+		File dir = new File(path);
+		if(!dir.exists()) {
+			dir.mkdirs();
+		}
+		//生成uuid作为文件名称
+		String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+		String imgName = uuid+file.getOriginalFilename();
+		String imgType = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1, 
+				file.getOriginalFilename().length());
+		System.out.println("path:"+path+" name:"+imgName+" type: "+imgType);
+		if(!file.isEmpty() && imgType.toLowerCase().equals("jpg")) {
+			//保存图片至服务器
+			file.transferTo(new File(dir.getAbsolutePath()+File.separator+imgName));
+			System.out.println("upload img success!");
+			map.addAttribute("code", "200");
+			String basePath = request.getSession().getServletContext().getRealPath("/uploadImgs/");
+			System.out.println("result path:"+basePath+File.separator+imgName);
+			map.addAttribute("result", "/uploadImgs"+File.separator+imgName);
+		}else {
+			System.out.println("upload img failed!");
+			map.addAttribute("code", "400");
+		}
+		return map;
 	}
 
 }
